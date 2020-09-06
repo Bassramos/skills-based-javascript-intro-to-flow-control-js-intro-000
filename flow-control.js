2fa2a053a482e75bd7bd4f55e80fb4bca69b7da6
Joshua Ramos <bassramos@gmail.com>
2020-09-06 03:35:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,13 +14,13 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age>=13&&age<=19)  {
+  if (age >= 13 && age <= 19)  {
     return "You are a teenager!"
-  }  else if (age>13) {
+  }  else if (age > 13) {
       return "You are a kid"
-    }else if{  (age<19)} {
+  } else if  (age < 19 ) {
     return "You are a grownup"
-      }
+  }
 }
 function ternaryTeenager(age) {
 
